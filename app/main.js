9206335cb37bfbff7241c42ac6117d066ca829e7
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2024-11-18 11:16:55 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,23 @@
-import "./style.css";
+//
+//gets data
+//shows data
+async function getData() {
+  // fetch returns a promise
+  try {
+    const response = await fetch(
+      "https://inshorts.deta.dev/news?category=science"
+    );
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.category;
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("Article not found");
+  }
+}
+getData();
+//not working
