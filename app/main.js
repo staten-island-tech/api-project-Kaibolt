429493bb7cf968c58eb5b4697d5fb9541b7900c9
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2024-11-18 22:35:49 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -4,10 +4,13 @@
 const URL =
   "https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=tmOTqwdrNCT4wJ218FAcoGKribFSalPv";
 async function getData(URL) {
-  // fetch returns a promise
   try {
     const response = await fetch(URL);
     const data = await response.json();
+    console.log(data.results);
+    // This all works fine for now
+    // map through data.results for articles, then run a function to insert everything into the document
+    //Or I can go through the results and for each article, , insert it as HTML. I like this idea as it's simple.
   } catch (error) {
     console.log(error);
   }
