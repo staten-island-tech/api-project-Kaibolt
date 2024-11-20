7c0536a7462e379b60ffb188cb573da7c79ac610
Kaibolt <kyle.salowitz08@gmail.com>
2024-11-20 12:30:35 -0500

    ascf

--- app/main.js
+++ app/main.js
@@ -12,9 +12,19 @@ async function getData(URL) {
     // map through data.results for articles, then run a function to insert everything into the document
     //Or I can go through the results and for each article, , insert it as HTML. I like this idea as it's simple.
     data.results.forEach((article) => {
-      document.body.insertAdjacentHTML(
+      document.querySelector(".flex").insertAdjacentHTML(
         "beforeend",
-        `<b>${article.title}</b> <br>`
+        `<div class="card card-compact bg-base-100 w-96 shadow-xl">
+        <figure>
+          <img
+            src="${article.multimedia[1].url}"
+            alt="${article.multimedia[1].caption}" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title"><a href="${article.url}">${article.title}</a></h2>
+          <p>${article.abstract}</p>
+        </div>
+      </div>`
       );
     });
   } catch (error) {
